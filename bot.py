55e4356fb52ad9119f53f5d158203177073512c0
Mango <evan940415@gmail.com>
2021-06-06 18:04:06 +0800

    upload sth

---
+++ bot.py
@@ -0,0 +1,27 @@
+import discord
+import json
+import os
+from discord.ext import commands
+from datetime import datetime
+
+with open("./setting/Token.json", "r") as TokenFile:
+    TokenData = json.load(TokenFile)
+
+with open("./setting/botSetting.json", "r") as SettingFile:
+    SettingData = json.load(SettingFile)
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix=SettingData["Prefix"], intents=intents)
+
+@bot.event
+async def on_ready():
+    now = f"[{datetime.now().strftime('%Y/%m/%d %H:%M:%S')}]"
+    BotName = SettingData["BotName"]
+    print(f"{now} >> {BotName} is on ready <<")
+
+for Filename in os.listdir("./commands"):
+    if Filename.endswith(".py"):
+        bot.load_extension(f"commands.{Filename[:-3]}")
+
+if __name__ == "__main__":
+    bot.run(TokenData["Token"])
---
+++ commands/load.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+from core.classes import Cog_Extension
+from datetime import datetime
+
+def loadLog(loadType, author, extension):
+    now = f"[{datetime.now().strftime('%Y/%m/%d %H:%M:%S')}]"
+    out = f"{now} {author} {loadType} {extension}.py"
+    return out
+
+class load(Cog_Extension):
+    @commands.command()
+    async def load(self, ctx, extension):
+        print(loadLog("load", ctx.author, extension))
+        self.bot.load_extension(f'commands.{extension}')
+
+    @commands.command()
+    async def unload(self, ctx, extension):
+        print(loadLog("unload", ctx.author, extension))
+        self.bot.unload_extension(f'commands.{extension}')
+
+    @commands.command()
+    async def reload(self, ctx, extension):
+        print(loadLog("reload", ctx.author, extension))
+        self.bot.reload_extension(f'commands.{extension}')
+
+def setup(bot):
+    bot.add_cog(load(bot))
